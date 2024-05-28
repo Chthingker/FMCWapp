41b838fc0a2a393b94f90afd8f49026f1db28abb
Chen Ming <2290804909@qq.com>
2024-05-28 12:44:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,19 @@
 import streamlit as st
-# import torch
+import torch
 st.write(
     """
     # welcome **world!**
     """
 )
 
+
+
+# 选择数据：
+option = st.selectbox(
+    "请选择一个样例文件进行监测",
+    ("0", "1", "2"))
+
+# st.write("You selected:", option)  option返回选择的数
+selected_file_num = int(option) 
+
+st.write(option.type)
